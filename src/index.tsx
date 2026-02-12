a2e48f2d670a3b31e1a8e85b58bc4d0c258dc1e2
Sam Collins <oscollins@gmail.com>
2026-02-12 05:06:36 -0800

    Change default name/DOB to Oscar and 2017-02-20.

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,8 @@ import Main from "./main";
 import Footer from "./footer";
 
 const params = new URLSearchParams(window.location.search);
-const name = params.get("name") ?? process.env.DEFAULT_NAME ?? "Birthday Star";
-const dob = params.get("dob") ?? process.env.DEFAULT_DOB ?? "2017-01-01";
+const name = params.get("name") ?? process.env.DEFAULT_NAME ?? "Oscar";
+const dob = params.get("dob") ?? process.env.DEFAULT_DOB ?? "2017-02-20";
 function App() {
   return (
     <div className="max-w-[1024px] mx-auto">
